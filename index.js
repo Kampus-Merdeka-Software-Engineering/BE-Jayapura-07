1cb2bdf1899ba02175360a1647dc9842c9609022
akeylafe <audreyakeyla01@gmail.com>
2023-10-09 17:24:17 +0700

    Updated

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ app.use(cors());
 app.use("/api/products", productRouter);
 app.use("/api/transaction", transactionRouter);
 
-app.listen(process.env.APP_PORT, () => {
-    console.log("Express API running in port: " + process.env.APP_PORT);
-  });
+app.listen(process.env.APP_PORT || 3000, () => {
+    console.log("Express API running in port: " + (process.env.APP_PORT || 3000));
+});
+
