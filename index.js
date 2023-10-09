6f1ee543114a3aabcdec7f8404c50252860f754b
akeylafe <audreyakeyla01@gmail.com>
2023-10-09 16:37:36 +0700

    Updated

--- index.js
+++ index.js
@@ -9,15 +9,6 @@ const productRouter = require('./app/routes/product-routes');
 const transactionRouter = require('./app/routes/transaction-routes');
 const { sequelize } = require("./app/models");
 
-const conn = new sequelize.Sequelize("mysql://avnadmin:AVNS_aPDvO8-2hjmhfvNaDWc@vetemens-captone-jayapura-07.aivencloud.com:28237/defaultdb?ssl-mode=REQUIRED",{
-    ssl: fs.readFileSync(path.join(__dirname, 'ca.pem')),
-    dialect: 'mysql',
-    logging: false
-
-}
-)
-
-
 app.use(express.json()); // supaya express bisa response json
 app.use(cors());
 // http router prefix
